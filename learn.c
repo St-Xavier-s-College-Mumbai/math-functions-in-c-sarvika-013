1aa007d917c0edffa4692e5be33d7a9183b4cac3
sarvika-013 <rajulasarvika@gmail.com>
2024-09-09 19:07:39 +0000

    first program

---
+++ learn.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+  int n;
+    printf("hello world\n");
+    printf("enter the number:");
+    scanf("%d",&n);
+     if (n%2==0){
+      printf("the above number is even");
+     }
+    else {
+       printf("the above number is odd");
+      }
+ 
+ return 0;
+
+}
